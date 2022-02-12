4cb3b65699d5c193bb759cd03f7e5333194a48d3
Sergio Baptista <sergio.baptista@akqa.com>
2022-02-12 13:32:27 -0500

    added route handling to home spaces - office space from initial view

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -6,6 +6,27 @@ router.add('home', () => { console.log('home'); })
 router.add('frag1', () => { console.log('frag1'); })
 router.add('frag2', () => { console.log('frag2'); })
 
+router.add('homeSpace-homeOffice', () => {
+    let initview = document.getElementsByClassName("ws-initial-view")[0];
+    if (initview && !initview.classList.contains('explored') && !initview.classList.contains('displayNone')) {
+
+        let listener = () => {
+            initview.classList.toggle('displayNone');
+            initview.removeEventListener('animationend', listener);
+          }
+        initview.addEventListener('animationend', listener);
+        initview.classList.toggle('explored');
+    }
+
+    let spacesview = document.getElementsByClassName("ws-room-view")[0];
+    if (spacesview && spacesview.classList.contains('displayNone')) {
+        spacesview.classList.toggle('displayNone');
+    }
+    if (spacesview && !spacesview.classList.contains('displayBlock')) {
+        spacesview.classList.toggle('displayBlock');
+    }
+})
+
 window.addEventListener('hashchange', (ev) => {
   let path = location.hash === '' ? 'home' : location.hash.substring(1);
   router.route(path);
