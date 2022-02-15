13a8963b7193a9a840fa0294bc3bd45dbe278c20
Sergio Baptista <sergio.baptista@akqa.com>
2022-02-15 12:04:48 -0500

    styled and animated workspace selector

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,5 +1,5 @@
 import Router from "./router";
-console.log('Hello world');
+var workspaces = require('../../data/workspaces.json');
 
 const router = new Router();
 router.add('home', () => { console.log('home'); })
@@ -10,11 +10,31 @@ router.add('/homeSpace', () => {
     fromHomeToRooms();
 })
 
+router.add('/officeSpace', () => {
+    
+})
+
+router.add('/anywhere', () => {
+    
+})
+
+
+
 window.addEventListener('hashchange', (ev) => {
   let path = location.hash === '' ? 'home' : location.hash.substring(1);
   router.route(path);
+  updateNav(path);
 });
 
+window.addEventListener('resize', () => {
+    updateNav(location.hash.substring(1));
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(()=>{
+        updateNav(location.hash.substring(1));
+    }, 300)
+});
 
 const fromHomeToRooms = function (){
     let initview = document.getElementsByClassName("ws-initial-view")[0];
@@ -58,4 +78,48 @@ const fromHomeToRooms = function (){
         spacesview.style['zIndex'] = 0;
         initview.style['zIndex'] = 1;
     }
+}
+
+const updateNav = function (path) {
+
+    let mypath = path;
+    if (path.indexOf("/") > -1) {
+        let paths = path.split("/");
+        paths = paths.filter(word => word.length > 0);
+        mypath = paths[0];
+    }
+
+    let navLinks = document.querySelectorAll(".ws-workspace-nav a");
+    let navSelector = document.getElementById("ws-nav-underline");
+
+    let selectedIndex = -1;
+    
+    workspaces.forEach((element, index) => {
+        if (element.slug == mypath) {
+            selectedIndex = index;
+        }
+    });
+
+    console.log(selectedIndex);
+
+    if (selectedIndex === -1 && !navSelector.classList.contains("ws-displayNone")) {
+        navSelector.classList.add("ws-displayNone");
+        navSelector.style.left = "50%";
+        navSelector.style.width = "0px";
+        return
+    } else {
+        navSelector.classList.remove("ws-displayNone");
+    }
+
+    let left = navLinks[selectedIndex].offsetLeft;
+    let width = navLinks[selectedIndex].offsetWidth;
+    setTimeout(()=>{
+        navSelector.style.left = left + "px";
+        navSelector.style.width = width + "px";
+    }, 1)
+    
+}
+
+const updateRoomsSelector = function (path){
+
 }
